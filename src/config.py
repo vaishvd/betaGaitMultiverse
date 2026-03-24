c0a8cd52d975836e1ffe43794e22904350ec92b8
Vaishali <vaishalivinod97@gmail.com>
2026-03-24 11:22:27 +0100

    Update config.py with file directories for scripts 05 to 08

--- src/config.py
+++ src/config.py
@@ -14,7 +14,12 @@ dir_proj = Path(__file__).resolve().parents[1]
 # Define paths for data directories and work notebooks
 DIR_DATA = define_dir(dir_proj, "data") # Data folder
 DIR_RAWDATA = define_dir(DIR_DATA, "d00_raw") # Raw datasets
+DIR_MONTAGE = define_dir(DIR_DATA, "d00_montage") # Raw datasets with montage set
 DIR_SEG = define_dir(DIR_DATA, "d01_segmented") # Datasets segmented with events of interest
 DIR_SIGCLEAN = define_dir(DIR_DATA, "d02_sigclean") # Data after signal cleaning
 DIR_PREICA = define_dir(DIR_DATA, "d03_preica") # Data before ICA
 DIR_ICA = define_dir(DIR_DATA, "d04_ica") # Data after ICA
+DIR_GAIT = define_dir(DIR_DATA, "d05_gaitcycles") # Extracted and time-normalized gait cycles
+DIR_TFR = define_dir(DIR_DATA, "d06_tfr")
+DIR_ERSP = define_dir(DIR_DATA, "d07_ersp")
+DIR_PLOT = define_dir(DIR_DATA, "d09_plotgaitbeta")
