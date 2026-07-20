00db4fa42e12a6623b829fa7339bc855175be866
Vaishali <vaishalivinod97@gmail.com>
2026-07-20 11:20:50 +0200

    Update dataset in download script from "splitbelt" to "jacobsen" and change dataset ID to "ds003039"

--- scripts/00_download_openneuro_dataset.py
+++ scripts/00_download_openneuro_dataset.py
@@ -2,13 +2,12 @@ from src.config import DATASETS
 from src.paths import get_dataset_dirs
 import openneuro as on
 
-DATASET = "splitbelt"
+DATASET = "jacobsen"
 
 cfg  = DATASETS[DATASET]
 dirs = get_dataset_dirs(DATASET)
 
 on.download(
-    dataset="ds004475",
+    dataset="ds003039",
     target_dir=dirs["raw"],
-    exclude=["**/anat", "**/headmodel"],
 )
